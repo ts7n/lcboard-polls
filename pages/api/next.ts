12801e211c4ea55b6890ff88116a25e6f3098a60
ts7n <65257768+ts7n@users.noreply.github.com>
2023-12-10 17:09:01 -0600

    always allow refresh

--- pages/api/next.ts
+++ pages/api/next.ts
@@ -6,12 +6,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const authHeader = req?.headers?.authorization;
-  // if (
-  //   !process.env.CRON_SECRET ||
-  //   authHeader !== `Bearer ${process.env.CRON_SECRET}`
-  // ) {
-  //   return res.status(401).json({ success: false, msg: 'Unauthorized' });
+  const authHeader = req?.headers?.authorization;
+  if (
+    !process.env.CRON_SECRET ||
+    authHeader !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).json({ success: false, msg: 'Unauthorized' });
+  }
+
+  // const now = new Date();
+  // if (now.getUTCHours() < 15 || now.getUTCHours() > 20) {
+  //   return res.status(200).json({ success: true, result: 'offline' });
   // }
 
   const current = (await axios.get('https://api.challonge.com/v1/tournaments/favoritelatinlunchpreview/matches.json', {
